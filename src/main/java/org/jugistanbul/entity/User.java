3f0d3ea1b17a4b91e4fc771aada124d12a417707
hakdogan <hakdogan75@gmail.com>
2021-10-19 20:55:39 +0300

    SignInResource added

---
+++ src/main/java/org/jugistanbul/entity/User.java
@@ -0,0 +1,41 @@
+package org.jugistanbul.entity;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import io.quarkus.security.jpa.Password;
+import io.quarkus.security.jpa.Roles;
+import io.quarkus.security.jpa.UserDefinition;
+import io.quarkus.security.jpa.Username;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.util.List;
+
+import io.quarkus.logging.Log;
+
+/*
+ * @author hakdogan (huseyin.akdogan@patikaglobal.com)
+ * Created on 18.10.2021
+ */
+@Entity
+@Table(name = "user_t")
+@UserDefinition
+public class User extends PanacheEntity
+{
+    @Username
+    public String username;
+
+    @Password
+    public String password;
+
+    @Roles
+    public String role;
+
+    public static User findByUsername(final String username){
+        return find("username", username).firstResult();
+    }
+
+    public static List<User> findByRole(final String role){
+        return find("role", role).list();
+    }
+
+}
---
+++ src/main/java/org/jugistanbul/resource/SignInResource.java
@@ -0,0 +1,87 @@
+package org.jugistanbul.resource;
+
+import io.smallrye.jwt.build.Jwt;
+import io.vertx.core.http.HttpServerRequest;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.jboss.resteasy.annotations.jaxrs.PathParam;
+import org.jugistanbul.entity.User;
+import org.jboss.logging.Logger;
+import org.wildfly.security.password.PasswordFactory;
+import org.wildfly.security.password.WildFlyElytronPasswordProvider;
+import org.wildfly.security.password.interfaces.BCryptPassword;
+import org.wildfly.security.password.util.ModularCrypt;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.*;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Date;
+
+/*
+ * @author hakdogan (huseyin.akdogan@patikaglobal.com)
+ * Created on 18.10.2021
+ */
+@Path("/api")
+@RequestScoped
+public class SignInResource
+{
+    @Inject
+    Logger log;
+
+    @ConfigProperty(name = "mp.jwt.verify.issuer", defaultValue = "https://jugistanbul.org/issuer")
+    String issuer;
+
+    @GET
+    @Path("/signIn/{username}/{password}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response signIn(@PathParam String username, @PathParam String password,
+                           @Context HttpServerRequest httpServerRequest) {
+
+        var user = User.findByUsername(username);
+        return user != null && verifyBCryptPassword(user.password, password)
+                ? createSuccessLoginResponse(user, httpServerRequest) : createFailedLoginResponse();
+
+    }
+
+    public boolean verifyBCryptPassword(String bCryptPasswordHash, String passwordToVerify) {
+
+        try {
+            var provider = new WildFlyElytronPasswordProvider();
+            var passwordFactory = PasswordFactory.getInstance(BCryptPassword.ALGORITHM_BCRYPT, provider);
+            var userPasswordDecoded = ModularCrypt.decode(bCryptPasswordHash);
+            var userPasswordRestored = passwordFactory.translate(userPasswordDecoded);
+            return passwordFactory.verify(userPasswordRestored, passwordToVerify.toCharArray());
+
+        } catch (NoSuchAlgorithmException | InvalidKeyException | InvalidKeySpecException e) {
+            log.error("Verification failed!", e);
+        }
+        return false;
+    }
+
+    private Response createSuccessLoginResponse(final User user, final HttpServerRequest httpServerRequest) {
+        var token = Jwt.issuer(issuer)
+                .upn(user.username)
+                .claim("userId", user.id)
+                .groups(user.role)
+                .expiresIn(Duration.ofMinutes(30))
+                .sign();
+
+        return Response.ok(token, MediaType.TEXT_PLAIN_TYPE)
+                .header(HttpHeaders.AUTHORIZATION, token)
+                .expires(Date.from(Instant.now().plus(Duration.ofMinutes(30))))
+                .cookie(new NewCookie("auth-token", token, "/", httpServerRequest.remoteAddress().hostName(),
+                        null, 60 * 30, false)).build();
+
+    }
+
+    private Response createFailedLoginResponse() {
+        return Response.status(Response.Status.UNAUTHORIZED).build();
+    }
+}
