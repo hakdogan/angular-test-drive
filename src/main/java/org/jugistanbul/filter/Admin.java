2c013c08c60b0fbc733dc0bd23bba456b3e983d4
hakdogan <hakdogan75@gmail.com>
2021-10-24 18:18:45 +0300

    Admin authentication filter added

---
+++ src/main/java/org/jugistanbul/filter/Admin.java
@@ -0,0 +1,13 @@
+package org.jugistanbul.filter;
+
+import javax.ws.rs.NameBinding;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/*
+ * @author hakdogan (huseyin.akdogan@patikaglobal.com)
+ * Created on 24.10.2021
+ */
+@NameBinding
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Admin { }
