34aa6857a2206811086e9b1e2ed87046a4cc3c68
hakdogan <hakdogan75@gmail.com>
2021-11-05 22:16:30 +0300

    WebSecurityFilter updated

--- src/main/java/org/jugistanbul/security/filter/WebSecurityFilter.java
+++ src/main/java/org/jugistanbul/security/filter/WebSecurityFilter.java
@@ -1,6 +1,5 @@
 package org.jugistanbul.security.filter;
 
-import io.vertx.core.http.HttpServerRequest;
 import org.jboss.logging.Logger;
 import org.jugistanbul.security.PermissionProvider;
 
@@ -8,13 +7,11 @@ import javax.inject.Inject;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.container.PreMatching;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.Provider;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.Optional;
 
 /*
  * @author hakdogan (huseyin.akdogan@patikaglobal.com)
@@ -24,15 +21,11 @@ import java.util.Optional;
 @PreMatching
 public class WebSecurityFilter implements ContainerRequestFilter
 {
-
     @Inject
     Logger log;
 
     @Inject
     PermissionProvider permissionProvider;
-
-    @Context
-    HttpServerRequest httpServerRequest;
 
     @Override
     public void filter(ContainerRequestContext context) throws IOException {
@@ -54,10 +47,5 @@ public class WebSecurityFilter implements ContainerRequestFilter
             context.abortWith(Response.status(Response.Status.UNAUTHORIZED).build());
             return;
         }
-
-        var principal = Optional.ofNullable(context.getSecurityContext().getUserPrincipal());
-        log.info(String.format("Request %s %s from IP %s User %s", context.getMethod(), path,
-                httpServerRequest.remoteAddress().toString(),
-                principal.isPresent()?principal.get().getName():"Anonymous"));
     }
 }
