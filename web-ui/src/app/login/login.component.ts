b62327218588255e7dd5d7ec70a0466d7e9ffad1
Hüseyin Akdoğan <hakdogan75@gmail.com>
2022-02-22 23:13:48 +0300

    login.component.ts updated

--- web-ui/src/app/login/login.component.ts
+++ web-ui/src/app/login/login.component.ts
@@ -28,19 +28,22 @@ export class LoginComponent implements OnInit {
 
   onSubmit(formDTO: LoginFormDTO): boolean {
 
-    this.http
-        .post(this.apiURL, formDTO, { responseType: 'text'})
-        .subscribe(response => {
-          console.log(response);
-          window.location.href = "/user-list";
-        }, error => {
-          this.errorMessage = error.statusText === 'Unauthorized'
-            ? 'Wrong username or password!'
-            : error.statusText;
-
-          console.log(error.statusText);
-        });
+    this.http.post(this.apiURL, formDTO, { responseType: 'text'})
+      .subscribe({
+        next: this.handleUpdateResponse.bind(this),
+        error: this.handleError.bind(this)
+      });
 
       return false;
+  }
+
+  private handleUpdateResponse(): void {
+    window.location.href = "/user-list";
+  }
+
+  private handleError(error: any): void {
+    this.errorMessage = error.statusText === 'Unauthorized'
+      ? 'Wrong username or password!'
+      : error.statusText;
   }
 }
