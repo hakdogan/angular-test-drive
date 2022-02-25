1bad3ff16eeac1e9e1c19a4d18741536bd0c3b3f
Hüseyin Akdoğan <hakdogan75@gmail.com>
2022-02-25 23:03:04 +0300

    user-list component updated

--- web-ui/src/app/user-list/user-list.component.ts
+++ web-ui/src/app/user-list/user-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {environment} from "../../environments/environment";
+import {HttpClient} from '@angular/common/http';
+import {UserDTO} from "./user.model";
 
 @Component({
   selector: 'app-user-list',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  userList!: UserDTO[];
+
+  readonly apiURL = environment.baseURL + "/api/user/all";
 
   ngOnInit(): void {
+    this.callUsers();
   }
 
+  callUsers(){
+    console.log("********** Call Users **********");
+    console.log(this.apiURL);
+
+    this.http.get<UserDTO[]>(this.apiURL)
+      .subscribe({
+        next: this.renderTable.bind(this),
+        error: this.handleError.bind(this)
+      });
+  }
+
+  private renderTable(users: UserDTO[]): void {
+    this.userList = users;
+    console.log(users);
+  }
+
+  private handleError(error: any): void {
+    console.log(error.statusText);
+  }
 }
